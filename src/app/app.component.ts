f6027d7a6b9a19500cedd45cf1513c38c480ecf5
Edric Chan <edric.chan.1997@gmail.com>
2018-06-23 13:21:42 +0800

    chore: initial blog website

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, ViewChild, OnInit } from '@angular/core';
+import { SidenavService } from './sidenav.service';
+import { MatSidenav } from '@angular/material/sidenav';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'blog-app',
+	templateUrl: './app.component.html'
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+	@ViewChild('sidenav') sidenav: MatSidenav;
+	@ViewChild('postsSidenav') postsSidenav: MatSidenav;
+	posts: any;
+	constructor(
+		private sidenavService: SidenavService,
+		private http: HttpClient
+	) {
+	}
+	ngOnInit() {
+		this.sidenavService.postsSidenav = this.postsSidenav;
+		this.sidenavService.sidenav = this.sidenav;
+		this.http.get('assets/blogposts.json').subscribe(result => {
+			this.posts = result;
+		});
+	}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,41 @@
+import { SidenavService } from './sidenav.service';
+import { AppRouting } from './app.routing';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-
-
+import { MaterialModule } from './material.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import {
+	PostViewerComponent,
+	TagViewerComponent
+} from './blogposts';
 
+import 'hammerjs';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		HomeComponent,
+		PostViewerComponent,
+		TagViewerComponent
+	],
+	imports: [
+		BrowserModule,
+		BrowserAnimationsModule,
+		FormsModule,
+		ReactiveFormsModule,
+		HttpClientModule,
+		MaterialModule,
+		AppRouting,
+		FlexLayoutModule
+	],
+	providers: [
+		SidenavService
+	],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
